75bf8c3556b88ac401d2553ffc4ac0eae4224128
Vlad <k-vlad-k@mail.ru>
2017-09-05 19:56:52 +0300

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
   resources  :projects
+  resources  :users
   root "projects#index"
   root "projects#show"
 end
