b7894fa9cb4fea4636f2aeae9d43a664f6509965
Vlad <k-vlad-k@mail.ru>
2017-09-22 06:47:49 +0300

    template fixes

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,7 @@ class Project < ApplicationRecord
 
   validate :checkDateGraterThanNow
 
-  has_attached_file :image, :styles => { medium: "300x300#" }
+  has_attached_file :image, styles: {square: "300x300#", normal: "400x400>", smallest: "150x150#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def remainingsDays
